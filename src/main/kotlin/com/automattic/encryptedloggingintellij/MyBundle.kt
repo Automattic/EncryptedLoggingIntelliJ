160508f1adb137558059d761112b2f5d2b5c40be
Wojtek Zieba <wojtek.zieba@automattic.com>
2024-05-10 10:38:58 +0200

    Update package and coordinates to reference Automattic

--- src/main/kotlin/com/automattic/encryptedloggingintellij/MyBundle.kt
+++ src/main/kotlin/com/automattic/encryptedloggingintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package com.github.wzieba.encryptedloggingintellij
+package com.automattic.encryptedloggingintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/automattic/encryptedloggingintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/automattic/encryptedloggingintellij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package com.github.wzieba.encryptedloggingintellij.services
+package com.automattic.encryptedloggingintellij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.wzieba.encryptedloggingintellij.MyBundle
+import com.automattic.encryptedloggingintellij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
